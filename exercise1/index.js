30baa1b8fea4d69f1303cacb7861b86bdc5b8c6a
unknown <Administrator@PAPGROUP.LOCAL>
2021-12-14 15:05:19 +0330

    use filter

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,14 +1,6 @@
 function evenNumber(array) {
 
-    let result = [];
-
-    for (const number of array) {
-        const element = number % 2;
-        if (element == 0)
-            // console.log(number);
-            result.push(number)
-    }
-        return result
+ return array.filter(number=> number%2 ===0)
 }
 
 const data = [2, 9, 3, 10, 5, 4]
